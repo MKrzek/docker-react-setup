8804bbf2eaa1a65958639180d5f64d244ad6793e
MKrzek <mkrzek@googlemail.com>
2020-04-17 12:11:34 +0100

    feat: updated layout

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hellooooooo!
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react
         </a>
       </header>
     </div>
